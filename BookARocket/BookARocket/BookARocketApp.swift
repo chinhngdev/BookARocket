f9d2d815985bc11fb7add15ce44942de7d19b970
ChinhNg <22429215+chinhngdev@users.noreply.github.com>
2025-04-21 22:50:06 +0700

    Init project

---
+++ BookARocket/BookARocket/BookARocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookARocketApp.swift
+//  BookARocket
+//
+//  Created by Chinh on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct BookARocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
